6c2259a422b3b3df8d40750b5cb6b20552f79a91
doni <donioctavian1016@gmail.com>
2021-03-08 11:17:28 +0700

    Basic Activity

--- app/src/main/java/com/enigmacamp/belajarkotlin/MainActivity.kt
+++ app/src/main/java/com/enigmacamp/belajarkotlin/MainActivity.kt
@@ -2,10 +2,20 @@ package com.enigmacamp.belajarkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+//    lateinit var nameTextView : TextView(Java Style)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        nameTextView = findViewById(R.id.nameTextView)(Java Style)
+//        nameTextView.text = "Doni Octavian"(Java Style)
+        //Kotlin Style
+        nameTextView.text = "Doni Octavian"
+
     }
 }
